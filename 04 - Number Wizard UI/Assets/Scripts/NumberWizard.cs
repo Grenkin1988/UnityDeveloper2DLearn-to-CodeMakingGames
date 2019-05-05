ac46c3be11b5c8dbc645f2e61bb910e2ae90d6f6
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-05 20:38:59 +0300

    NumberWizard works with UI

---
+++ 04 - Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+    [SerializeField]
+    private int _maxNumber;
+    [SerializeField]
+    private int _minNumber;
+    private int _guess;
+
+    [SerializeField]
+    private TMP_Text _guessText;
+
+    public void OnPressHigher() {
+        _minNumber = _guess;
+        NextGuess();
+    }
+
+    public void OnPressLower() {
+        _maxNumber = _guess;
+        NextGuess();
+    }
+
+    private void UpdateGuessText() {
+        _guessText.text = $"{_guess}";
+    }
+
+    private void Start() {
+        StartGame();
+    }
+
+    private void StartGame() {
+        _maxNumber = _maxNumber + 1;
+        NextGuess();
+    }
+
+    private void NextGuess() {
+        _guess = (_maxNumber + _minNumber) / 2;
+        UpdateGuessText();
+    }
+}
