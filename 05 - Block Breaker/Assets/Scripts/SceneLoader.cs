bca289e5700dd4acfdc18e5cb43610764931572e
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-05 22:49:18 +0300

    Scenes prepared

---
+++ 05 - Block Breaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public void LoadNextScene() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
