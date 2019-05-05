224b547770c0ecc39356dac086dfd6d7828eb699
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-05 17:10:40 +0300

    Game is ready

---
+++ 03-Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+    [SerializeField]
+    private Text _textComponent;
+
+    [SerializeField]
+    private State _startingState;
+
+    private State _state;
+
+    private void Start() {
+        _state = _startingState;
+        _textComponent.text = _state?.GetStoryText();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            QuitGame();
+        }
+        ManageState();
+    }
+
+    private void QuitGame() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void ManageState() {
+        State[] nextStates = _state?.GetNextStates();
+        if(nextStates == null || nextStates.Length == 0) { return; }
+        for (int i = 0; i < nextStates.Length; i++) {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i)) {
+                _state = nextStates[i];
+                _textComponent.text = _state?.GetStoryText();
+            }
+        }
+    }
+}
---
+++ 03-Text101/Assets/Scripts/State.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+    [TextArea(10, 14)]
+    [SerializeField]
+    private string _storyText;
+
+    [SerializeField]
+    private State[] _nextStates;
+
+    public string GetStoryText() {
+        return _storyText;
+    }
+
+    public State[] GetNextStates() {
+        return _nextStates;
+    }
+}
