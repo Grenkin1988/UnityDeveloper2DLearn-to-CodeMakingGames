50d285d6e74fc54e5a8ebc7cf6f046af02245632
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-06 19:28:40 +0300

    Level 1 is done

---
+++ 05 - Block Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Destroy(gameObject);
+    }
+}
