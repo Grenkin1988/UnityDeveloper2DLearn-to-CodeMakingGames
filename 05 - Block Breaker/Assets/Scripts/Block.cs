ced30bb6cd9085c56bf4dca08ae3c0a13cc409e8
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-07 22:51:36 +0300

    More blocks variant

--- 05 - Block Breaker/Assets/Scripts/Block.cs
+++ 05 - Block Breaker/Assets/Scripts/Block.cs
@@ -1,16 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Block : MonoBehaviour {
     [SerializeField]
     private AudioClip breakSound;
     [SerializeField]
-    private GameObject blockSparkleVFX;
+    private GameObject blockSparkleVFX;   
+    [SerializeField]
+    private Sprite[] hitSprites;
 
     private Level level;
     private GameSession gameStatus;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private int timesHit;
+    private int maxHits;
 
     private void Start() {
         gameStatus = FindObjectOfType<GameSession>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        maxHits = hitSprites != null ? hitSprites.Length + 1 : 1;
         CountBlock();
     }
 
@@ -23,7 +33,28 @@ public class Block : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (tag == "Breakable") {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit() {
+        timesHit++;
+        if (timesHit >= maxHits) {
             DestroyBlock();
+        } else {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite() {
+        if(hitSprites == null || hitSprites.Length == 0) {
+            return;
+        }
+        int spriteIndex = Mathf.Clamp(timesHit - 1, 0, hitSprites.Length - 1);
+        if (hitSprites[spriteIndex] != null) {
+            spriteRenderer.sprite = hitSprites[spriteIndex];
+        } else {
+            Debug.LogError($"GameObject[{gameObject.name}] do not have sprite in hitSprites on index {spriteIndex}", gameObject);
         }
     }
 
