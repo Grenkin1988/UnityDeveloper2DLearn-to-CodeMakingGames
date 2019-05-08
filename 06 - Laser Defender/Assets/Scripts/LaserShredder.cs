fa462223092bd52e0a52b7a67479aaf1d8e44e56
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-09 00:09:08 +0300

    Player can fire

---
+++ 06 - Laser Defender/Assets/Scripts/LaserShredder.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class LaserShredder : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D col) {
+        Destroy(col.gameObject);
+    }
+}
--- 06 - Laser Defender/Assets/Scripts/Player.cs
+++ 06 - Laser Defender/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
@@ -8,12 +9,19 @@ public class Player : MonoBehaviour {
     private float _paddingX = 1f;
     [SerializeField]
     private float _paddingY = 1f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _projectileSpeed = 10f;
+    [SerializeField]
+    private float _projectileFiringPeriod = 0.2f;
 
     private Camera _gameCamera;
     private float _xMin;
     private float _xMax;
     private float _yMin;
     private float _yMax;
+    private Coroutine _firingCoroutine;
 
     // Start is called before the first frame update
     private void Start() {
@@ -21,16 +29,10 @@ public class Player : MonoBehaviour {
         SetUpMoveBoundaries();
     }
 
-    private void SetUpMoveBoundaries() {
-        _xMin = _gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + _paddingX;
-        _xMax = _gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - _paddingX;
-        _yMin = _gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + _paddingY;
-        _yMax = _gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - _paddingY;
-    }
-
     // Update is called once per frame
     private void Update() {
         Move();
+        Fire();
     }
 
     private void Move() {
@@ -41,5 +43,29 @@ public class Player : MonoBehaviour {
         float newYPos = Mathf.Clamp(transform.position.y + deltaY, _yMin, _yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void Fire() {
+        if (Input.GetButtonDown("Fire1")) {
+            _firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1")) {
+            StopCoroutine(_firingCoroutine);
+        }
+    }
+
+    private IEnumerator FireContinuously() {
+        while (true) {
+            GameObject laser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, _projectileSpeed);
+            yield return new WaitForSeconds(_projectileFiringPeriod);
+        }
+    }
+
+    private void SetUpMoveBoundaries() {
+        _xMin = _gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + _paddingX;
+        _xMax = _gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - _paddingX;
+        _yMin = _gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + _paddingY;
+        _yMax = _gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - _paddingY;
     }
 }
