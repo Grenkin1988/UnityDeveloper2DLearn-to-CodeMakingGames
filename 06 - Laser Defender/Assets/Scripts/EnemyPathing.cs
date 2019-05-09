668aec8d01772646d5de70058305c0b4c04835ff
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-09 18:50:19 +0300

    Enemy can move on path

---
+++ 06 - Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+    [SerializeField]
+    private WaveConfig _waveConfig;
+    [SerializeField]
+    private float _moveSpeed = 2f;
+
+    private int _waypointIndex = 0;
+    private List<Transform> _waypoints;
+
+    // Start is called before the first frame update
+    private void Start() {
+        _waypoints = new List<Transform>(_waveConfig.GetWaypoints());
+        transform.position = _waypoints[_waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        Move();
+    }
+
+    private void Move() {
+        if (_waypointIndex <= _waypoints.Count - 1) {
+            Vector3 targetPosition = _waypoints[_waypointIndex].transform.position;
+            float movementThisFrame = _moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition) {
+                _waypointIndex++;
+            }
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 06 - Laser Defender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject {
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _pathPrefab;
+    [SerializeField]
+    private float _timeBetweenSpawns = 0.5f;
+    [SerializeField]
+    private float _spawnRandomFactor = 0.3f;
+    [SerializeField]
+    private int _numberOfEnemies = 5;
+    [SerializeField]
+    private float _moveSpeed = 2f;
+
+    public GameObject EnemyPrefab => _enemyPrefab;
+    public GameObject PathPrefab => _pathPrefab;
+    public float TimeBetweenSpawns => _timeBetweenSpawns;
+    public float SpawnRandomFactor => _spawnRandomFactor;
+    public int NumberOfEnemies => _numberOfEnemies;
+    public float MoveSpeed => _moveSpeed;
+
+    public IEnumerable<Transform> GetWaypoints() {
+        foreach (Transform child in PathPrefab.transform) {
+            yield return child;
+        }   
+    }
+}
