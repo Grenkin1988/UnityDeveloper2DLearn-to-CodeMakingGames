c83fec88c6818f2c31376e8c63c6e738b08aee5e
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-10 00:05:39 +0300

    Enemy can spawn

--- 06 - Laser Defender/Assets/Scripts/EnemyPathing.cs
+++ 06 - Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -2,21 +2,20 @@
 using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour {
-    [SerializeField]
     private WaveConfig _waveConfig;
-    [SerializeField]
-    private float _moveSpeed = 2f;
-
     private int _waypointIndex = 0;
     private List<Transform> _waypoints;
 
-    // Start is called before the first frame update
-    private void Start() {
+    public void SetWaveConfig(WaveConfig waveConfig) {
+        _waveConfig = waveConfig;
+        SetupWaypoints();
+    }
+
+    private void SetupWaypoints() {
         _waypoints = new List<Transform>(_waveConfig.GetWaypoints());
         transform.position = _waypoints[_waypointIndex].transform.position;
     }
 
-    // Update is called once per frame
     private void Update() {
         Move();
     }
@@ -24,7 +23,7 @@ public class EnemyPathing : MonoBehaviour {
     private void Move() {
         if (_waypointIndex <= _waypoints.Count - 1) {
             Vector3 targetPosition = _waypoints[_waypointIndex].transform.position;
-            float movementThisFrame = _moveSpeed * Time.deltaTime;
+            float movementThisFrame = _waveConfig.MoveSpeed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
             if (transform.position == targetPosition) {
                 _waypointIndex++;
---
+++ 06 - Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    [SerializeField]
+    private List<WaveConfig> _waveConfigs;
+
+    private int _startingWave;
+
+    // Start is called before the first frame update
+    private void Start() {
+        WaveConfig currentWave = _waveConfigs[_startingWave];
+        StartCoroutine(SpawnAllWaves());
+    }
+
+    private IEnumerator SpawnAllWaves() {
+        foreach (WaveConfig config in _waveConfigs) {
+            Coroutine startWave = StartCoroutine(SpawnAllEnemiesInWave(config));
+            yield return startWave;
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig) {
+        for (int i = 0; i < waveConfig.NumberOfEnemies; i++) {
+            GameObject enemy = Instantiate(waveConfig.EnemyPrefab, waveConfig.GetWaypoints().First().position, Quaternion.identity);
+            var pathing = enemy.GetComponent<EnemyPathing>();
+            pathing.SetWaveConfig(waveConfig);
+            yield return new WaitForSeconds(waveConfig.TimeBetweenSpawns);
+        }
+    }
+}
