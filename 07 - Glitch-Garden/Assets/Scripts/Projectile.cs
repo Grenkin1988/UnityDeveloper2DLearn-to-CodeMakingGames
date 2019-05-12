dfca6856cb5557de49620c64661438b8ff8fcd53
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-12 21:26:39 +0300

    Move projectile

---
+++ 07 - Glitch-Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    [SerializeField]
+    private float _speed = 5f;
+
+    private void Update() {
+        transform.Translate(Vector2.right * _speed * Time.deltaTime);
+    }
+}
--- 07 - Glitch-Garden/Assets/Scripts/Shooter.cs
+++ 07 - Glitch-Garden/Assets/Scripts/Shooter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
     [SerializeField]
-    private GameObject _projectilePrefab;
+    private Projectile _projectilePrefab;
     [SerializeField]
     private Transform _gunPosition;
 
