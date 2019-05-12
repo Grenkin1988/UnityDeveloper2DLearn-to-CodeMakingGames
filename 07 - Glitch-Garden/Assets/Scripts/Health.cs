8b56d7f2617afa01ba6e6f9e484a3cc3f0d2eb7b
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-12 22:23:14 +0300

    Implementing Health & Damage

---
+++ 07 - Glitch-Garden/Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour {
+    [SerializeField]
+    private float _health = 100f;
+
+    public void DealDamage(float damage) {
+        _health -= damage;
+        if(_health <= 0) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Destroy(gameObject);
+    }
+}
--- 07 - Glitch-Garden/Assets/Scripts/Projectile.cs
+++ 07 - Glitch-Garden/Assets/Scripts/Projectile.cs
@@ -3,8 +3,25 @@
 public class Projectile : MonoBehaviour {
     [SerializeField]
     private float _speed = 5f;
+    [SerializeField]
+    private float _damage = 100f;
 
     private void Update() {
         transform.Translate(Vector2.right * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Health health = other.gameObject.GetComponent<Health>();
+        ProcessHit(health);
+    }
+
+    private void ProcessHit(Health health) {
+        if (health == null) { return; }
+        health.DealDamage(_damage);
+        Die();
+    }
+
+    private void Die() {
+        Destroy(gameObject);
     }
 }
