71ad6a286e1985dbc820da3db9bc5c2059fd970c
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-12 21:19:32 +0300

    Gun position

---
+++ 07 - Glitch-Garden/Assets/Scripts/Shooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    [SerializeField]
+    private GameObject _projectilePrefab;
+    [SerializeField]
+    private Transform _gunPosition;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+
+    public void Fire() {
+        Instantiate(_projectilePrefab, _gunPosition.transform.position, _gunPosition.transform.rotation);
+    }
+}
