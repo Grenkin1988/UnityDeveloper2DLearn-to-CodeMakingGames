87f23971236881d690c75a1e57f8b320a8f49dcc
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-12 19:59:04 +0300

    Spawn Attackers Using Coroutine

---
+++ 07 - Glitch-Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+    [Range(0f, 10f), SerializeField]
+    private float _moveSpeed = 1f;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        transform.Translate(Vector2.left * _moveSpeed * Time.deltaTime);
+    }
+}
---
+++ 07 - Glitch-Garden/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+    [SerializeField]
+    private float _minSpawnDelay = 1f;
+    [SerializeField]
+    private float _maxSpawnDelay = 5f;
+    [SerializeField]
+    private Attacker _attackerPrefab;
+
+    private bool _doSpawn = true;
+
+    private IEnumerator Start() {
+        while (_doSpawn) {
+            yield return new WaitForSeconds(Random.Range(_minSpawnDelay, _maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker() {
+        Instantiate(_attackerPrefab, transform.position, transform.rotation);
+    }
+
+    private void Update() {
+
+    }
+}
