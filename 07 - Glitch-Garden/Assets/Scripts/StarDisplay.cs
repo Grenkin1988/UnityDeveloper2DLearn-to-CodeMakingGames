e1ed143f6cf66f2814beeaf8d22ac239ed9dafb9
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-15 21:54:00 +0300

    resource system

---
+++ 07 - Glitch-Garden/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour {
+    [SerializeField]
+    private int _stars = 100;
+
+    private Text _starText;
+
+    public void AddStars(int stars) {
+        _stars += stars;
+        UpdateDisplay();
+    }
+
+    public void RemoveStars(int stars) {
+        if (_stars >= stars) {
+            _stars -= stars;
+            UpdateDisplay();
+        }
+    }
+
+    private void Start() {
+        _starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay() {
+        _starText.text = $"{_stars}";
+    }
+}
