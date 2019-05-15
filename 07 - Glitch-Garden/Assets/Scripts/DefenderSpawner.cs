480a0db7c03d454c7551fff23a5792141f79205a
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-15 20:25:06 +0300

    Spawn At Mouse Position

---
+++ 07 - Glitch-Garden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _defender;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnMouseDown() {
+        Vector2 worldPos = GetSquareClicked();
+        SpawnDefender(worldPos);
+    }
+
+    private Vector2 GetSquareClicked() {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
+    private void SpawnDefender(Vector2 worldPos) {
+        
+        GameObject newDefender = Instantiate(_defender, worldPos, Quaternion.identity);
+    }
+}
