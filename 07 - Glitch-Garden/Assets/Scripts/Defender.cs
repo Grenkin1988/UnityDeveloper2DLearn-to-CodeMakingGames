84cb0e4bb46ed9eb8ccd24609f04aa80e442a3cc
Grenkin1988 <kruglovgleb@gmail.com>
2019-05-23 21:43:58 +0300

    Add Resources As Anim Event

--- 07 - Glitch-Garden/Assets/Scripts/Defender.cs
+++ 07 - Glitch-Garden/Assets/Scripts/Defender.cs
@@ -3,4 +3,8 @@
 public class Defender : MonoBehaviour {
     [SerializeField]
     private int _starCost = 100;
+
+    public void AddStars(int amount) {
+        FindObjectOfType<StarDisplay>().AddStars(amount);
+    }
 }
